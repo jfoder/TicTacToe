ea390a6cbd4c86846003791c15e41e7ca57c6086
jfoder <jaroslaw.foder@gmail.com>
2018-09-28 19:12:48 +0200

    Initial commit

---
+++ java/com/jfoder/tictactoe/MainActivity.java
@@ -0,0 +1,79 @@
+package com.jfoder.tictactoe;
+
+import android.content.res.Resources;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private int currentContentView; //0 - activity_main, 1 - 3x3 game, 2 - 4x4 game, 3 - 5x5 game
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        currentContentView = 0;
+    }
+
+    public void onButtonClick(View v) {
+        Log.d("DEBUG", "onButtonClick called");
+        if(v.getId() == R.id.button3x3) {
+            setContentView(R.layout.game3x3);
+            currentContentView = 1;
+            Log.d("DEBUG", "3x3 button clicked");
+            Button[][] buttons = getButtonsArray(3);
+            TextView t1 = (TextView) findViewById(R.id.playerRound);
+            TextView t2 = (TextView) findViewById(R.id.roundSymbol);
+            TTTGame game3x3 = new TTTGame(3, buttons, t1, t2);
+        }
+        if(v.getId() == R.id.button4x4) {
+            setContentView(R.layout.game4x4);
+            currentContentView = 2;
+            Log.d("DEBUG", "4x4 button clicked");
+            Button[][] buttons = getButtonsArray(4);
+            TextView t1 = (TextView) findViewById(R.id.playerRound);
+            TextView t2 = (TextView) findViewById(R.id.roundSymbol);
+            TTTGame game3x3 = new TTTGame(4, buttons, t1, t2);
+        }
+        if(v.getId() == R.id.button5x5) {
+            setContentView(R.layout.game5x5);
+            currentContentView = 3;
+            Log.d("DEBUG", "5x5 button clicked");
+            Button[][] buttons = getButtonsArray(5);
+            TextView t1 = (TextView) findViewById(R.id.playerRound);
+            TextView t2 = (TextView) findViewById(R.id.roundSymbol);
+            TTTGame game3x3 = new TTTGame(5, buttons, t1, t2);
+        }
+    }
+
+    private Button[][] getButtonsArray(int gameSize) {
+        Button[][] result = new Button[gameSize][gameSize];
+        String seeked;
+        Resources res = getResources();
+        for(int i = 0; i < gameSize; i++) {
+            for(int j = 0; j < gameSize; j++){
+                seeked = "button" + gameSize + "_" + i + j;
+                Log.d("DEBUG", seeked);
+                int buttonId = res.getIdentifier(seeked, "id", getApplicationContext().getPackageName());
+                result[i][j] = (Button) findViewById(buttonId);
+                Log.d("DEBUG", "ID: " + result[i][j].getId());
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public void onBackPressed() {
+        Log.d("DEBUG", "Back button pressed");
+        if(currentContentView != 0) {
+            setContentView(R.layout.activity_main);
+            currentContentView = 0;
+        }
+        else{
+            System.exit(0);
+        }
+    }
+}
---
+++ java/com/jfoder/tictactoe/TTTGame.java
@@ -0,0 +1,171 @@
+package com.jfoder.tictactoe;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class TTTGame{
+    private int gameSize;
+    private int moves;
+    private final int MAX_MOVES;
+    private ButtonState[][] buttonsState;
+    private GameState round;
+    private TextView whoseRound;
+    private TextView roundSymbol;
+
+    private enum GameState{
+        CIRCLE_MOVE,
+        CROSS_MOVE,
+        CIRCLE_WIN,
+        CROSS_WIN,
+        DRAW
+    }
+    private enum FieldState{
+        EMPTY,
+        CIRCLE,
+        CROSS
+    }
+    public class ButtonState{
+        private FieldState btnState;
+        private Button button;
+        public ButtonState(Button b)
+        {
+            btnState = FieldState.EMPTY;
+            button = b;
+        }
+
+        public FieldState getButtonState() {
+            return btnState;
+        }
+
+        public void setButtonState(FieldState state) {
+            btnState = state;
+        }
+
+        public Button getButton() {return button;}
+    }
+
+    public TTTGame(int gameSize, Button[][] buttons, TextView whoseRound, TextView roundSymbol){
+        this.gameSize = gameSize;
+        this.whoseRound = whoseRound;
+        this.roundSymbol = roundSymbol;
+        this.round = GameState.CIRCLE_MOVE;
+        moves = 0;
+        MAX_MOVES = gameSize * gameSize;
+        buttonsState = new ButtonState[gameSize][gameSize];
+        for(int i = 0; i < gameSize; i++){
+            for(int j = 0; j < gameSize; j++){
+                Log.d("DEBUG", ""+ buttons[0][0].getId());
+                buttonsState[i][j] = new ButtonState(buttons[i][j]);
+            }
+        }
+        setOnClickListeners();
+        updateGameState();
+    }
+
+    private void setOnClickListeners() {
+        for(int i = 0; i < gameSize; i++){
+            for(int j = 0; j < gameSize; j++){
+                Button b = buttonsState[i][j].getButton();
+                final ButtonState btn = buttonsState[i][j];
+                b.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(round == GameState.CIRCLE_MOVE && btn.btnState == FieldState.EMPTY){
+                            ((Button)v).setText("O");
+                            btn.btnState = FieldState.CIRCLE;
+                            round = GameState.CROSS_MOVE;
+                            moves++;
+                            updateGameState();
+                        }
+                        else if(round == GameState.CROSS_MOVE && btn.btnState == FieldState.EMPTY){
+                            ((Button)v).setText("X");
+                            btn.btnState = FieldState.CROSS;
+                            round = GameState.CIRCLE_MOVE;
+                            moves++;
+                            updateGameState();
+                        }
+                    }
+                });
+            }
+        }
+    }
+
+    private void updateGameState(){
+        for(int i = 0; i < gameSize; i++) {
+            for(int j = 0; j < gameSize; j++) {
+                if(checkIfInsideLine(i, j)){
+                    if(buttonsState[i][j].btnState == FieldState.CIRCLE) round = GameState.CIRCLE_WIN;
+                    else round = GameState.CROSS_WIN;
+                    break;
+                }
+            }
+        }
+        if(round == GameState.CIRCLE_MOVE && moves < MAX_MOVES) roundSymbol.setText("O");
+        else if(round == GameState.CROSS_MOVE && moves < MAX_MOVES) roundSymbol.setText("X");
+        else if(round == GameState.CIRCLE_WIN){
+            whoseRound.setText("Wygrał gracz:");
+            roundSymbol.setText("O");
+        }
+        else if(round == GameState.CROSS_WIN){
+            whoseRound.setText("Wygrał gracz:");
+            roundSymbol.setText("X");
+        }
+        else if(moves == MAX_MOVES) {
+            round = GameState.DRAW;
+            whoseRound.setText("Remis!");
+            roundSymbol.setText("");
+        }
+
+    }
+
+    private boolean checkIfInsideLine(int i, int j) {
+        Log.d("DEBUG", "Checking method called");
+        if(gameSize == 3 || gameSize == 4) {
+            if (buttonsState[i][j].btnState != FieldState.EMPTY){
+                if(j + 2 < gameSize) {
+                    if(checkIfEqual(buttonsState[i][j], buttonsState[i][j+1], buttonsState[i][j+2])) return true;
+                }
+                if(i + 2 < gameSize) {
+                    if(checkIfEqual(buttonsState[i][j], buttonsState[i+1][j], buttonsState[i+2][j])) return true;
+                }
+                if(i + 2 < gameSize && j + 2 < gameSize) {
+                    if(checkIfEqual(buttonsState[i][j], buttonsState[i+1][j+1], buttonsState[i+2][j+2])) return true;
+                }
+                return false;
+            }
+            return false;
+        }
+        else if(gameSize == 5){
+            if (buttonsState[i][j].btnState != FieldState.EMPTY){
+                if(j + 3 < gameSize) {
+                    if(checkIfEqual(buttonsState[i][j], buttonsState[i][j+1], buttonsState[i][j+2], buttonsState[i][j+3])) return true;
+                }
+                if(i + 3 < gameSize) {
+                    if(checkIfEqual(buttonsState[i][j], buttonsState[i+1][j], buttonsState[i+2][j], buttonsState[i+3][j])) return true;
+                }
+                if(i + 3 < gameSize && j + 3 < gameSize) {
+                    if(checkIfEqual(buttonsState[i][j], buttonsState[i+1][j+1], buttonsState[i+2][j+2], buttonsState[i+3][j+3])) return true;
+                }
+                if(i + 3 < gameSize && j - 3 >= 0) {
+                    if(checkIfEqual(buttonsState[i][j], buttonsState[i+1][j-1], buttonsState[i+2][j-2], buttonsState[i+3][j-3])) return true;
+                }
+                return false;
+            }
+            return false;
+        }
+        return false;
+    }
+
+    private boolean checkIfEqual(ButtonState a, ButtonState b, ButtonState c) {
+        if(a.btnState == b.btnState && b.btnState == c.btnState) return true;
+        return false;
+    }
+    private boolean checkIfEqual(ButtonState a, ButtonState b, ButtonState c, ButtonState d) {
+        if(a.btnState == b.btnState && b.btnState == c.btnState && c.btnState == d.btnState) return true;
+        return false;
+    }
+}
